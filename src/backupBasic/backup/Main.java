fd0e81b5426685646310bd068792647d3b154b7e
Tobias Hotz <tobias.hotz@hotmail.de>
2016-11-29 20:51:30 +0100

    Fix crash when showing crash information

--- src/backupBasic/backup/Main.java
+++ src/backupBasic/backup/Main.java
@@ -13,8 +13,8 @@ import java.util.logging.Logger;
  * @author Tobias Hotz
  */
 public class Main implements Thread.UncaughtExceptionHandler {
-
     private static final Logger logger = i18n.getLogger(Main.class);
+
 	/**
 	 * Verwaltet das Logging-System und gibt dann an den ArgParser ab
 	 */
@@ -26,11 +26,20 @@ public class Main implements Thread.UncaughtExceptionHandler {
 
 	@Override
 	public void uncaughtException(Thread t, Throwable e) {
-		logger.severe(i18n.translate("UncaughtException")+ "\t Stacktrace:");
-		e.printStackTrace();
-		if(!GraphicsEnvironment.isHeadless()) {
-			JOptionPane.showMessageDialog(null, i18n.translate("UncaughtException") + "\n" + i18n.translate("Error") + ": "+ e +"\n" +  i18n.translate("Details"), "ERROR", JOptionPane.ERROR_MESSAGE);
-		}
+	    try {
+            logger.severe(i18n.translate("UncaughtException")+ "\t Stacktrace:");
+            e.printStackTrace();
+            if(!GraphicsEnvironment.isHeadless())
+                JOptionPane.showMessageDialog(null, i18n.translate("UncaughtException") +
+                        "\n" + i18n.translate("Error") + ": " + e + "\n" + i18n.translate("Details"), "ERROR", JOptionPane.ERROR_MESSAGE);
+            } catch(Exception e2) {
+	            e.printStackTrace();
+	            System.err.println("Errors while printing Error:");
+	            e2.printStackTrace();
+	            if(!GraphicsEnvironment.isHeadless())
+	            JOptionPane.showMessageDialog(null, "An fatal error occurred, and the program crashed." +
+                        "\nIn addition to that, it failed to translate this Message", "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
 		System.exit(-1);
 	}
 }
